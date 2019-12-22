7f8b78f085a59c0d5148659696ef13c418b242da
Marc Gorzala <marc.gorzala@zalando.de>
2019-12-22 10:54:20 +0100

    base from react, jpa, boot tutorial

---
+++ src/main/java/de/frubumi/dance/DanceApplication.java
@@ -0,0 +1,13 @@
+package de.frubumi.dance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/frubumi/dance/DanceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.frubumi.dance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
