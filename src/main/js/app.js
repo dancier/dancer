63c7fa88e70c287ec6c306e2d841ab8d435b9d98
Marc Gorzala <marc.gorzala@zalando.de>
2019-12-29 07:22:55 +0100

    add and remove dancers via react

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,74 +1,291 @@
-// tag::vars[]
+'use strict';
+
 const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
-// end::vars[]
 
-// tag.:app[]
+const follow = require('./follow'); // function to hop multiple links by "rel"
+
+const root = '/api';
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {dancers: []};
+		this.state = {dancers: [], attributes: [], pageSize: 2, links: {}};
+		this.updatePageSize = this.updatePageSize.bind(this);
+		this.onCreate = this.onCreate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
+		this.onNavigate = this.onNavigate.bind(this);
     }
 
-    componentDidMount() {
-        client({method: 'GET', path: '/api/dancers'}).done(response => {
-            this.setState({dancers: response.entity._embedded.dancers});
+    // tag::follow-2[]
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'dancers', params: {size: pageSize}}]
+        ).then(dancerCollection => {
+            return client({
+                method: 'GET',
+                path: dancerCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                return dancerCollection;
+            });
+        }).done(dancerCollection => {
+            this.setState({
+                dancers: dancerCollection.entity._embedded.dancers,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: dancerCollection.entity._links});
         });
     }
+    // end::follow-2[]
+
+	// tag::create[]
+	onCreate(newDancer) {
+		follow(client, root, ['dancers']).then(dancerCollection => {
+			return client({
+				method: 'POST',
+				path: dancerCollection.entity._links.self.href,
+				entity: newDancer,
+				headers: {'Content-Type': 'application/json'}
+			})
+		}).then(response => {
+			return follow(client, root, [
+				{rel: 'dancers', params: {'size': this.state.pageSize}}]);
+		}).done(response => {
+			if (typeof response.entity._links.last !== "undefined") {
+				this.onNavigate(response.entity._links.last.href);
+			} else {
+				this.onNavigate(response.entity._links.self.href);
+			}
+		});
+	}
+	// end::create[]
+
+	// tag::delete[]
+	onDelete(dancer) {
+		client({method: 'DELETE', path: dancer._links.self.href}).done(response => {
+			this.loadFromServer(this.state.pageSize);
+		});
+	}
+	// end::delete[]
+
+	// tag::navigate[]
+	onNavigate(navUri) {
+		client({method: 'GET', path: navUri}).done(dancerCollection => {
+			this.setState({
+				dancers: dancerCollection.entity._embedded.dancers,
+				attributes: this.state.attributes,
+				pageSize: this.state.pageSize,
+				links: dancerCollection.entity._links
+			});
+		});
+	}
+	// end::navigate[]
+
+	// tag::update-page-size[]
+	updatePageSize(pageSize) {
+		if (pageSize !== this.state.pageSize) {
+			this.loadFromServer(pageSize);
+		}
+	}
+	// end::update-page-size[]
+
+    // tag::follow-1[]
+    componentDidMount() {
+        this.loadFromServer(this.state.pageSize);
+    }
+    // end::follow-1[]
 
     render() {
         return (
-            <DancerList dancers={this.state.dancers}/>
+			<div>
+				<CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+				<DancerList dancers={this.state.dancers}
+							  links={this.state.links}
+							  pageSize={this.state.pageSize}
+							  onNavigate={this.onNavigate}
+							  onDelete={this.onDelete}
+							  updatePageSize={this.updatePageSize}/>
+			</div>
     )
     }
 }
 
-// end::app[]
+// tag::create-dialog[]
+class CreateDialog extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-// tag::dancer-list[]
-class DancerList extends React.Component{
+    handleSubmit(e) {
+        e.preventDefault();
+        const newDancer = {};
+        this.props.attributes.forEach(attribute => {
+            newDancer[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newDancer);
+
+        // clear out the dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it.
+        window.location = "#";
+    }
+
     render() {
-        const dancers = this.props.dancers.map(dancer =>
-            <Dancer key={dancer._links.self.href} dancer={dancer}/>
+        const inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+            <input type="text" placeholder={attribute} ref={attribute} className="field"/>
+            </p>
     );
+
         return (
-            <table>
-            <tbody>
-            <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-             <th>Dance</th>
-              </tr>
-        {dancers}
-    </tbody>
-        </table>
+            <div>
+            <a href="#createDancer">Create</a>
+
+            <div id="createDancer" className="modalDialog">
+            <div>
+            <a href="#" title="Close" className="close">X</a>
+
+            <h2>Create new dancer</h2>
+
+        <form>
+        {inputs}
+        <button onClick={this.handleSubmit}>Create</button>
+            </form>
+            </div>
+            </div>
+            </div>
     )
     }
-}
-// end::dancer-list[]
 
+}
+// end::create-dialog[]
+
+class DancerList extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.handleNavFirst = this.handleNavFirst.bind(this);
+		this.handleNavPrev = this.handleNavPrev.bind(this);
+		this.handleNavNext = this.handleNavNext.bind(this);
+		this.handleNavLast = this.handleNavLast.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+	}
+
+	// tag::handle-page-size-updates[]
+	handleInput(e) {
+		e.preventDefault();
+		const pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+		if (/^[0-9]+$/.test(pageSize)) {
+			this.props.updatePageSize(pageSize);
+		} else {
+			ReactDOM.findDOMNode(this.refs.pageSize).value =
+				pageSize.substring(0, pageSize.length - 1);
+		}
+	}
+	// end::handle-page-size-updates[]
+
+	// tag::handle-nav[]
+	handleNavFirst(e){
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.first.href);
+	}
+
+	handleNavPrev(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.prev.href);
+	}
+
+	handleNavNext(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.next.href);
+	}
+
+	handleNavLast(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.last.href);
+	}
+	// end::handle-nav[]
+
+	// tag::dancer-list-render[]
+	render() {
+		const dancers = this.props.dancers.map(dancer =>
+			<Dancer key={dancer._links.self.href} dancer={dancer} onDelete={this.props.onDelete}/>
+		);
+
+		const navLinks = [];
+		if ("first" in this.props.links) {
+			navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+		}
+		if ("prev" in this.props.links) {
+			navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+		}
+		if ("next" in this.props.links) {
+			navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+		}
+		if ("last" in this.props.links) {
+			navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+		}
+
+		return (
+			<div>
+				<input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+				<table>
+					<tbody>
+						<tr>
+							<th>First Name</th>
+							<th>Last Name</th>
+							<th>Description</th>
+							<th></th>
+						</tr>
+						{dancers}
+					</tbody>
+				</table>
+				<div>
+					{navLinks}
+				</div>
+			</div>
+		)
+	}
+	// end::dancer-list-render[]
+}
 
 // tag::dancer[]
-class Dancer extends React.Component{
-    render() {
-        return (
-            <tr>
-            <td>{this.props.dancer.firstName}</td>
-            <td>{this.props.dancer.lastName}</td>
-            <td>{this.props.dancer.description}</td>
-            </tr>
-    )
-    }
+class Dancer extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.handleDelete = this.handleDelete.bind(this);
+	}
+
+	handleDelete() {
+		this.props.onDelete(this.props.dancer);
 }
-// end::employee[]
 
+	render() {
+		return (
+			<tr>
+				<td>{this.props.dancer.firstName}</td>
+				<td>{this.props.dancer.lastName}</td>
+				<td>{this.props.dancer.dance}</td>
+				<td>
+					<button onClick={this.handleDelete}>Delete</button>
+				</td>
+			</tr>
+		)
+	}
+}
+// end::dancer[]
 
-// tag::render[]
 ReactDOM.render(
 <App />,
     document.getElementById('react')
 )
-// end::render[]
