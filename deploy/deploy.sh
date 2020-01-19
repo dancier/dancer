1acf5b3037315bec7c1555c3ebf9ea8ba19dd572
Marc Gorzala <marc.gorzala@zalando.de>
2020-01-19 21:32:29 +0100

    remove the api token file (security issue)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # shellcheck disable=SC1073
-export CLOUD_API_TOKEN=$(cat "./cloud-api-token")
-export PRIVATE_KEY_FILE="dancier.key"
+export CLOUD_API_TOKEN=$(cat "./deploy/cloud-api-token")
+export PRIVATE_KEY_FILE="./deploy/dancier.key"
 
-IP=$(./create_next_server.py)
+IP=$(./deploy/create_next_server.py)
 
 echo "This is the created ip: " $IP
 
@@ -23,10 +23,10 @@ do
 done
 
 echo "Copy init-instance script"
-scp -i $PRIVATE_KEY_FILE -oStrictHostKeyChecking=no  ./init-instance.sh root@"${IP}":/root/
+scp -i $PRIVATE_KEY_FILE -oStrictHostKeyChecking=no  ./deploy/init-instance.sh root@"${IP}":/root/
 
 echo "Invoke bootstrap"
-ssh -i $PRIVATE_KEY_FILE -oStrictHostKeyChecking=no root@"${IP}"/root/init-instance.sh
+ssh -i $PRIVATE_KEY_FILE -oStrictHostKeyChecking=no root@"${IP}" /root/init-instance.sh
 
 echo "Enable floating IP"
 ssh -i $PRIVATE_KEY_FILE -oStrictHostKeyChecking=no root@"${IP}" sudo ip addr add 116.202.177.122 dev eth0
