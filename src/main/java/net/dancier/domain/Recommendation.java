1fdaf501be2d75aab78b12fb1f193a98975245d6
Marc Gorzala <marc@gorzala.de>
2021-06-23 07:16:49 +0200

    Added Javadoc

--- src/main/java/net/dancier/domain/Recommendation.java
+++ src/main/java/net/dancier/domain/Recommendation.java
@@ -3,9 +3,12 @@ package net.dancier.domain;
 import net.dancier.domain.dance.Dancer;
 import net.dancier.domain.dance.School;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * Wraps something that could be recommended.
- * @param <T>
+ * Instances are created with {@link #of(Recommendable)}
+ * @param <T> the Class that implements {@link Recommendable}
  */
 public class Recommendation<T extends Recommendable> {
 
@@ -19,7 +22,17 @@ public class Recommendation<T extends Recommendable> {
     public Type type;
     public T payload;
 
-    public static Recommendation of(Recommendable recommendable) {
+    /**
+     * Factory method that creates new instances of
+     * a {@link Recommendation}
+     * @param recommendable used as the payload of the Recommendation
+     * @return in case a proper Recommendable provided by the client, a Recommendation is being returned,
+     *         with the recommendable as the payload and a type that is inferred by the concrete class
+     *         of the recommendable.
+     * @throws IllegalArgumentException in case a {@link Type} could not be inferred for the given recommendable,
+     *         an {@link IllegalArgumentException} is being thrown
+     */
+    public static Recommendation of(@NotNull  Recommendable recommendable) {
         if (recommendable instanceof Dancer) {
             return new Recommendation(Type.DANCER, recommendable);
         } else if (recommendable instanceof School) {
