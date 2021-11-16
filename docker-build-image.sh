38e7ef9d07e584c958cdcbf385c02018b34add74
Marc Gorzala <marc@gorzala.de>
2021-11-16 20:32:55 +0100

    change to boot

--- docker-build-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mvn package
-source ./version.conf
-
-docker build -t dancier/dancer:${VERSION} .
--- docker-run-image.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source ./version.conf
-docker run --rm --name dancer --env-file ./.env -v $(pwd)/config.yml:/config.yml -p 8080:8080 --net=host dancier/dancer:${VERSION}
