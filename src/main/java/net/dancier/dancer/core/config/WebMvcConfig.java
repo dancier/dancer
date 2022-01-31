fde3502747ea0bd75c46e492174325d4f3a23189
Marc Gorzala <marc@gorzala.de>
2022-01-31 12:00:41 +0100

    added cors config for local development

--- src/main/java/net/dancier/dancer/core/config/WebMvcConfig.java
+++ src/main/java/net/dancier/dancer/core/config/WebMvcConfig.java
@@ -1,9 +1,13 @@
 package net.dancier.dancer.core.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
