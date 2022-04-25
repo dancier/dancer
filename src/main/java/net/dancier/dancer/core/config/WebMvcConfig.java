35e618d195513ba79a4ca879fdcb2c00ec15624b
Marc Gorzala <marc@dancier.net>
2022-04-25 08:29:23 +0200

    added accidentally removed configuration annotation

--- src/main/java/net/dancier/dancer/core/config/WebMvcConfig.java
+++ src/main/java/net/dancier/dancer/core/config/WebMvcConfig.java
@@ -1,14 +1,16 @@
 package net.dancier.dancer.core.config;
 
-import liquibase.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @EnableWebMvc
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private static Logger log = LoggerFactory.getLogger(WebMvcConfig.class);
@@ -20,7 +22,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        log.info("Configuring CORS for: " + allowedOrigins);
+        log.info("Configuring CORS for: " + String.join(",", allowedOrigins));
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
