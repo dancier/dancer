145a91e0b33a9d683e1e0fd685ded52572942f4e
Marc Gorzala <marc@dancier.net>
2022-05-16 17:13:11 +0200

    request mapping

--- src/main/java/net/dancier/dancer/school/SchoolController.java
+++ src/main/java/net/dancier/dancer/school/SchoolController.java
@@ -1,7 +1,9 @@
 package net.dancier.dancer.school;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("school")
 public class SchoolController {
 }
