e48d7849b1673c963e5bb5bb3acf92c047de3186
Marc Gorzala <marc@dancier.net>
2022-10-04 18:17:46 +0200

    debug

--- src/main/java/net/dancier/dancer/authentication/AdminUserInitializer.java
+++ src/main/java/net/dancier/dancer/authentication/AdminUserInitializer.java
@@ -11,6 +11,7 @@ import net.dancier.dancer.authentication.service.AuthenticationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -18,6 +19,7 @@ import javax.transaction.Transactional;
 import java.util.Collection;
 import java.util.Optional;
 
+@Profile("prod")
 @Component
 @RequiredArgsConstructor
 public class AdminUserInitializer {
