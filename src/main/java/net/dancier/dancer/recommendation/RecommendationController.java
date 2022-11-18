3b639c39cb6ff155f0d37b3b39a45220c720ffd4
Marc Gorzala <marc@dancier.net>
2022-11-18 16:19:47 +0100

    debug

--- src/main/java/net/dancier/dancer/recommendation/RecommendationController.java
+++ src/main/java/net/dancier/dancer/recommendation/RecommendationController.java
@@ -3,6 +3,7 @@ package net.dancier.dancer.recommendation;
 import lombok.AllArgsConstructor;
 import net.dancier.dancer.core.exception.NotFoundException;
 import net.dancier.dancer.core.model.Recommendable;
+import net.dancier.dancer.recommendation.dto.Mapper;
 import net.dancier.dancer.recommendation.model.RecommendationWrapper;
 import net.dancier.dancer.security.AuthenticatedUser;
 import net.dancier.dancer.security.CurrentUser;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static net.dancier.dancer.authentication.Constants.ROLE_USER;
 
@@ -30,7 +32,7 @@ public class RecommendationController {
     public ResponseEntity getTopRecommendations(@CurrentUser AuthenticatedUser authenticatedUser) {
         List<RecommendationWrapper> recommendables =
                 recommendationService.getRecommendationsForDancerId(authenticatedUser.getDancerIdOrThrow());
-      return ResponseEntity.ok(recommendables);
+      return ResponseEntity.ok( recommendables.stream().map(Mapper::recommendationWrapper2ExposedRecommendationDto).collect(Collectors.toList()));
     };
 
     @ExceptionHandler({NotFoundException.class})
