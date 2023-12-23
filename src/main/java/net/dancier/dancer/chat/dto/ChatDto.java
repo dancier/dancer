1f3b71165b8a78ad37cb79ca9866f53c238a8784
Marc Gorzala <marc@dancier.net>
2023-12-23 20:38:46 +0100

    timezone

--- src/main/java/net/dancier/dancer/chat/dto/ChatDto.java
+++ src/main/java/net/dancier/dancer/chat/dto/ChatDto.java
@@ -1,5 +1,6 @@
 package net.dancier.dancer.chat.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.OffsetDateTime;
@@ -11,8 +12,8 @@ public class ChatDto {
     private UUID chatId;
     private List<UUID> participantIds;
 //2023-12-15T13:00:23.897543763Z
-//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     //private OffsetDateTime lastActivity;
     private MessageDto lastMessage;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private OffsetDateTime createdAt;
 }
