d076e36fa589186c09164b944dd5c3d9f398ab22
Marc Gorzala <marc@dancier.net>
2023-12-23 20:49:18 +0100

    format

--- src/main/java/net/dancier/dancer/chat/dto/ChatDto.java
+++ src/main/java/net/dancier/dancer/chat/dto/ChatDto.java
@@ -14,6 +14,8 @@ public class ChatDto {
 //2023-12-15T13:00:23.897543763Z
     //private OffsetDateTime lastActivity;
     private MessageDto lastMessage;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+//    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdAt;
 }
