4da38fae4048ec4a6ecbb98800db30cafd89c6ec
Marc Gorzala <marc@dancier.net>
2023-12-27 06:54:27 +0100

    send now from the outbox

--- src/main/java/net/dancier/dancer/messaging/TopicConfiguration.java
+++ src/main/java/net/dancier/dancer/messaging/TopicConfiguration.java
@@ -1,22 +1,16 @@
 package net.dancier.dancer.messaging;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Configuration
 public class TopicConfiguration {
     @Bean
-    public NewTopic profileUpdated() {
+    public KafkaAdmin.NewTopics createTopics() {
+        
         return new NewTopic("profile-updated", 1, (short) 1);
     }
 
